e400a5d290f79a2ed6f20095e5f00a1edb76d3b6
Hernan Meydac Jean <hernanmj@live.com.ar>
2014-06-02 08:06:38 -0300

    First Commit

---
+++ Source/Converters.Markdown/MarkdownConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace Converters.Markdown
+{
+    public class MarkdownConverter
+    {
+        public string ToHtml(string markdown)
+        {
+            var engine = new MarkdownDeep.Markdown();
+            var result = engine.Transform(markdown);
+            return result;
+        }
+    }
+}
---
+++ Source/Marker.WPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace Marker.WPF
+{
+    using Converters.Markdown;
+
+    using ScintillaNET;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var scintilla = new Scintilla();
+            this.HostControl.Child = scintilla;
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var converter = new MarkdownConverter();
+
+            var result = converter.ToHtml(this.SourceContent.Text);
+        }
+
+        public Scintilla SourceContent
+        {
+            get
+            {
+                return (Scintilla)this.HostControl.Child;
+            }
+        }
+    }
+}
